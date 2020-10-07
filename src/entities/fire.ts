a60c63ab6bef16b0cc20a9de7b8dafeb89392532
Egor Bedov <bedovegor2203@gmail.com>
2020-10-07 20:06:53 +0300

    fix fire

--- src/entities/fire.ts
+++ src/entities/fire.ts
@@ -2,24 +2,25 @@ import Game from 'Core/game';
 import {Boundaries, POINT} from 'Interfaces';
 import Designer from 'Core/designer';
 import TemporaryUnit from 'Core/units/temporaryUnit';
-import {getBonus, pointInBounds} from 'Utils/utils';
+import {boundsIntersect, getBonus, pointInBounds} from 'Utils/utils';
 import {UNIT_HEIGHT, UNIT_WIDTH} from 'Constants';
 import Bonus from './bonus';
 
 
 export default class Fire extends TemporaryUnit {
     private readonly replacedObstacle: boolean;
+    private readonly bounds: Boundaries;
 
     constructor(g: Game, pos: POINT, replacedObstacle: boolean) {
         super(g, pos, Designer.images.fire || Designer.images.error);
         this.replacedObstacle = replacedObstacle;
         this.timer = 500;
-        const bounds = new Boundaries(null, {h: UNIT_HEIGHT, w: UNIT_WIDTH, ...this.pos});
-        this.game.players.forEach(p => pointInBounds({...p.pos}, bounds) && p.remove());
-        this.game.enemies.forEach(e => pointInBounds({...e.pos}, bounds) && e.remove());
+        this.bounds = new Boundaries(null, {h: UNIT_HEIGHT, w: UNIT_WIDTH, ...this.pos});
     }
 
     public work(deltaTime: number): void {
+        this.game.players.forEach(p => boundsIntersect(new Boundaries(p, null), this.bounds) && p.remove());
+        this.game.enemies.forEach(e => boundsIntersect(new Boundaries(e, null), this.bounds) && e.remove());
         super.work(deltaTime);
         if (this.timer < 0) {
             this.remove();
